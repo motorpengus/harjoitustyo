a5dd6b6c65336df3e86b8325d9053547a8ab84ff
Joona Koskinen <joonakoskinen03@gmail.com>
2025-03-31 15:07:46 +0300

    1

---
+++ tyo/src/main/java/com/harjoitus/tyo/TyoApplication.java
@@ -0,0 +1,13 @@
+package com.harjoitus.tyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TyoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TyoApplication.class, args);
+	}
+
+}
